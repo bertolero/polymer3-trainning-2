3548159fab8e82f505c30fed92c1ad08661dd2c3
Bertol <bertol.pinheiro@gmail.com>
2020-06-21 14:38:46 -0300

    Move config files to specific folder and split webpack into more than one file.
    Signed-off-by: Bertol <bertol.pinheiro@gmail.com>

--- config/gulpfile.js
+++ config/gulpfile.js
@@ -8,8 +8,11 @@ let spawn = require('child_process').spawn;
 let del = require('del');
 
 // helper function
-const webpackHandler = (command, done) => {
-	const spawnedProcess = spawn(/^win/.test(process.platform) ? 'npm.cmd' : 'npm', ['run', command], { stdio: 'inherit' });
+const webpackHandler = (env, done) => {
+	const spawnedProcess = spawn(/^win/.test(process.platform) ? 'webpack.cmd' : 'webpack',
+		['--mode', env, `--env.NODE_ENV=${env}`, '--config', 'webpack.config.js', '--context', '../'],
+		{ stdio: 'inherit' });
+
 	spawnedProcess.on('close', code => {
 		if (code && code > 0) {
 			done(new Error(`Error running webpack. Code: ${code}`));
@@ -20,31 +23,31 @@ const webpackHandler = (command, done) => {
 
 // Delete /public Files
 gulp.task('clean:dist', function() {
-	return del('./public');
+	return del('../public', { force: true });
 });
 
 // copy manifest file
 gulp.task('copy:manifest', function() {
-	return gulp.src('./assets/view/manifest.json').pipe(gulp.dest('./public/'));
+	return gulp.src('../assets/view/manifest.json').pipe(gulp.dest('../public/'));
 });
 
 // copy electron
 gulp.task('copy:electron', function() {
-	return gulp.src('./assets/js/electron/**/*').pipe(gulp.dest('./public/'));
+	return gulp.src('../assets/js/electron/**/*').pipe(gulp.dest('../public/'));
 });
 
 // Compiles SCSS To CSS and copy to dest folder
 gulp.task('copy:styles', function() {
-	return gulp.src('assets/scss/**/*.scss')
+	return gulp.src('../assets/scss/**/*.scss')
 		.pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
 		.pipe(autoprefixer({ overrideBrowserslist: ['last 2 versions'] }))
-		.pipe(gulp.dest('./public/css'))
+		.pipe(gulp.dest('../public/css'))
 		.pipe(browserSync.stream());
 });
 
 // Minimise and copy images
 gulp.task('copy:imagemin', function() {
-	return gulp.src('assets/img/**/*')
+	return gulp.src('../assets/img/**/*')
 		.pipe(imagemin([
 			imagemin.gifsicle({ interlaced: true }),
 			imagemin.mozjpeg({ progressive: true }),
@@ -53,19 +56,19 @@ gulp.task('copy:imagemin', function() {
 				plugins: [{ removeViewBox: true }, { cleanupIDs: false }]
 			})
 		]))
-		.pipe(gulp.dest('./public/img'));
+		.pipe(gulp.dest('../public/img'));
 });
 
 // Use Webpack to compile latest Javascript to ES5
 // Webpack on Development Mode
-gulp.task('webpack:dev', done => webpackHandler('dev:webpack', done));
+gulp.task('webpack:dev', done => webpackHandler('development', done));
 // Webpack on Production Mode
-gulp.task('webpack:prod', done => webpackHandler('build:webpack', done));
+gulp.task('webpack:prod', done => webpackHandler('production', done));
 
 // Browser-sync to get live reload and sync with mobile devices
 gulp.task('browser-sync', function(done) {
 	browserSync.init({
-		server: './public',
+		server: '../public',
 		notify: false,
 		open: false, //change this to true if you want the browser to open automatically
 		injectChanges: false
@@ -86,17 +89,17 @@ gulp.task('default',
 		'build:dev',
 		'browser-sync',
 		function runningWatch(done) {
-			gulp.watch('./assets/js/components/**/*', gulp.parallel('webpack:dev'));
-			gulp.watch('./assets/scss/**/*', gulp.parallel('copy:styles'));
-			gulp.watch('./assets/img/**/*', gulp.parallel('copy:imagemin'));
-			gulp.watch('./assets/view/manifest.json', gulp.parallel('copy:manifest'));
-			gulp.watch(['./public/**/*', './public/*']).on('change', reload);
+			gulp.watch('../assets/js/components/**/*', gulp.parallel('webpack:dev'));
+			gulp.watch('../assets/scss/**/*', gulp.parallel('copy:styles'));
+			gulp.watch('../assets/img/**/*', gulp.parallel('copy:imagemin'));
+			gulp.watch('../assets/view/manifest.json', gulp.parallel('copy:manifest'));
+			gulp.watch(['../public/**/*', '../public/*']).on('change', reload);
 			done();
 		})
 );
 
 // This is the production build for your app
-gulp.task('prod',
+gulp.task('build:prod',
 	gulp.series(
 		'clean:dist',
 		gulp.parallel('webpack:prod', 'copy:styles', 'copy:imagemin', 'copy:manifest'))
@@ -105,7 +108,7 @@ gulp.task('prod',
 // electron build
 gulp.task('electron:watch', function runElectronWatch(done) {
 	console.log('electron:watch');
-	const spawnedProcess = spawn(/^win/.test(process.platform) ? 'nodemon.cmd' : 'nodemon', ['--watch','public','--exec', 'electron', './public/electron-app.js'], { stdio: 'inherit' });
+	const spawnedProcess = spawn(/^win/.test(process.platform) ? 'nodemon.cmd' : 'nodemon', ['--watch', 'public', '--exec', 'electron', '../public/electron-app.js'], { stdio: 'inherit' });
 	spawnedProcess.on('close', code => {
 		if (code && code > 0) {
 			done(new Error(`Error running nodemon. Code: ${code}`));
@@ -117,11 +120,11 @@ gulp.task('electron:watch', function runElectronWatch(done) {
 gulp.task(
 	'default:electron',
 	gulp.series('build:dev', 'copy:electron', function runningWatch(done) {
-		gulp.watch('./assets/js/components/**/*', gulp.parallel('webpack:dev'));
-		gulp.watch('./assets/scss/**/*', gulp.parallel('copy:styles'));
-		gulp.watch('./assets/img/**/*', gulp.parallel('copy:imagemin'));
-		gulp.watch('./assets/js/electron/**/*', gulp.parallel('copy:electron'));
-		gulp.watch('./assets/view/manifest.json', gulp.parallel('copy:manifest'));
+		gulp.watch('../assets/js/components/**/*', gulp.parallel('webpack:dev'));
+		gulp.watch('../assets/scss/**/*', gulp.parallel('copy:styles'));
+		gulp.watch('../assets/img/**/*', gulp.parallel('copy:imagemin'));
+		gulp.watch('../assets/js/electron/**/*', gulp.parallel('copy:electron'));
+		gulp.watch('../assets/view/manifest.json', gulp.parallel('copy:manifest'));
 		done();
 	}, 'electron:watch')
 );
---
+++ config/webpack.config.app.common.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+	entry: {
+		main: './assets/js/components/app-polymer.ts'
+	},
+	output: {
+		path: path.resolve(__dirname, '../public'),
+		filename: '[name].js'
+	},
+	resolve: {
+		// see below for an explanation // '[name].[chunkhash].js' put this if you want to get hashed files to cache bust
+		extensions: ['.ts', '.js']
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				exclude: /node_modules/,
+				use: ['ts-loader', 'prettier-loader']
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					'style-loader',
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					'sass-loader',
+					'postcss-loader'
+				]
+			}
+		]
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: 'styles.css' // 'style.[contenthash].css' put this if you want to get hashed files to cache bust
+		}),
+		new HtmlWebpackPlugin({
+			hash: true,
+			template: 'assets/view/index.html'
+		}),
+		new WebpackMd5Hash(),
+		new CopyWebpackPlugin({
+			patterns: [{
+				from: '**/*.js',
+				to: 'js/webcomponents',
+				context: 'node_modules/@webcomponents/webcomponentsjs'
+			}]
+		})
+	]
+};
