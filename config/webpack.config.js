6b08f9deeffea0000d65bcf20744ddf15510ea7c
Bertol <bertol.pinheiro@gmail.com>
2020-06-21 16:24:07 -0300

    Remove comment.
    Signed-off-by: Bertol <bertol.pinheiro@gmail.com>

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -2,9 +2,6 @@ const webpackMerge = require('webpack-merge');
 const commonConfig = require('./webpack.config.app.common');
 
 module.exports = (env) => {
-
-	console.log(JSON.stringify(env.NODE_ENV))
-
 	const envConfig = require(`./webpack.config.app.${env.NODE_ENV}.js`);
 
 	return webpackMerge(commonConfig, envConfig);
